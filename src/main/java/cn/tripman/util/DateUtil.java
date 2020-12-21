76424bc559f4c6c3e9960ceb709b6c94952f6b10
liuyub <liuyub@trip.com>
2020-12-21 14:55:01 +0800

    update

--- src/main/java/cn/tripman/util/DateUtil.java
+++ src/main/java/cn/tripman/util/DateUtil.java
@@ -209,4 +209,29 @@ public class DateUtil {
         }
         return 0;
     }
+
+    public static int getSeaSon(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int month = cal.get(Calendar.MONTH);
+        switch (month) {
+            case 2:
+            case 3:
+            case 4:
+                return 1;
+            case 5:
+            case 6:
+            case 7:
+                return 2;
+            case 8:
+            case 9:
+            case 10:
+                return 3;
+            case 11:
+            case 0:
+            case 1:
+                return 4;
+        }
+        return 0;
+    }
 }
