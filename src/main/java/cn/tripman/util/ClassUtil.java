8136974c0a38c146a7254d4701a380bbe72900ab
liuyub <liuyub@trip.com>
2021-02-02 18:41:26 +0800

    feat:update

--- src/main/java/cn/tripman/util/ClassUtil.java
+++ src/main/java/cn/tripman/util/ClassUtil.java
@@ -1,11 +1,17 @@
 package cn.tripman.util;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author hero
@@ -42,4 +48,58 @@ public class ClassUtil {
         return fieldSet;
     }
 
+    /**
+     * 获取method
+     */
+    public static Method getMethod(ProceedingJoinPoint point) {
+        return ((MethodSignature) point.getSignature()).getMethod();
+    }
+
+    /**
+     * 取出第i个list
+     */
+    public static int getListIndex(Object[] args) {
+        if (args == null || args.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof List) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 空字符串和null会有冲突，这个默认忽略
+     */
+    public static String join(Object[] args) {
+        StringBuilder builder = new StringBuilder();
+        if (args == null) {
+            return builder.toString();
+        }
+        for (Object object : args) {
+            if (object == null) {
+                builder.append("_");
+            } else {
+                builder.append(object.toString()).append("_");
+            }
+        }
+        return builder.substring(0, builder.length() - 1);
+    }
+
+    /**
+     * 参数替换
+     */
+    public static <T> void replaceFirstList(Object[] args, List<T> list) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof List) {
+                args[i] = list;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(join(new Object[]{"string", null, "ddds"}));
+    }
 }
