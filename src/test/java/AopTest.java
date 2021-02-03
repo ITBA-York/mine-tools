e070dff1fea0e0ef419aac00a4d652e68cc59466
liuyub <liuyub@trip.com>
2021-02-03 19:48:58 +0800

    feat:update

--- src/test/java/AopTest.java
+++
@@ -1,9 +0,0 @@
-import cn.tripman.util.FileUtil;
-
-import java.io.File;
-
-public class AopTest extends NativeAop {
-    public static void main(String[] args) throws Exception {
-        System.out.println(FileUtil.readLastEmpty(new File("F:\\id.csv")));
-    }
-}
